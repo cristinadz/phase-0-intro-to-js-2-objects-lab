134077303a767e3c1ccd06c0dd66008131ce2ae9
+ <diazcristina.la@gmail.com>
2022-04-27 21:22:25 -0700

    commit tree 6805d1a8a42f6ea21faa09dacc118e9e1d159800

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+  name: "Paco",
+  streetAddress: "1234 Pleasant",
+};
+
+function updateEmployeeWithKeyAndValue(employeeObject, key, value) {
+  const updatedEmployee = { ...employeeObject };
+  updatedEmployee[key] = value;
+  return updatedEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newDelete = { ...employee };
+  delete newDelete[key];
+  return newDelete;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+// destructivelyDeleteFromEmployeeByKey(): this function should work the same as deleteFromEmployeeByKey() but it should mutate the employee Object.
